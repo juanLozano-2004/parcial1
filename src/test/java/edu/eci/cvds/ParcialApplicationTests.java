df59ff5a9ac91255fc2fbea228537b149b3bcdc1
juanLozano-2004 <juan.lcardenas@mail.escuelaing.edu.co>
2025-03-25 21:05:45 -0500

    Actualizar spring

---
+++ src/test/java/edu/eci/cvds/ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
